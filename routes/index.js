ff3113f7ff2d95662b6f48718c5695534f5074a8
dzapeka <douson@gmail.com>
2024-02-18 14:42:26 +0100

    Add authentication to filters API endpoints

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const filterRouter = require('./filters');
 
 const router = express.Router();
 
+const authMiddleware = require('../middlewares/auth');
+
 router.use('/auth', authRouter);
-router.use('/filters', filterRouter);
+router.use('/filters', authMiddleware, filterRouter);
 
 module.exports = router;
