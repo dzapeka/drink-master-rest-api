948822bc00afcdf34be07576ef28b705944e3665
dzapeka <douson@gmail.com>
2024-02-27 01:17:29 +0100

    Make user name a required field in update schema

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,17 @@ const express = require('express');
 const UserController = require('../controllers/user');
 const validateSchema = require('../middlewares/validateSchema');
 const upload = require('../middlewares/upload');
-const { userSubscribeSchema } = require('../schemas/user');
+const { userSubscribeSchema, userUpdateSchema } = require('../schemas/user');
 
 const router = express.Router();
 
 router.get('/current', UserController.currentUser);
-router.patch('/update', upload.single('avatar'), UserController.updateUser);
+router.patch(
+  '/update',
+  upload.single('avatar'),
+  validateSchema(userUpdateSchema),
+  UserController.updateUser
+);
 router.post(
   '/subscribe',
   validateSchema(userSubscribeSchema),
